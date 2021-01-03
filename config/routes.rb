5e0f82aa82286aeb58eb5b451167492e60df2b69
Rūta Šipkauskaitė <s033859@ad.viko.lt>
2021-01-03 02:18:52 +0200

    generate devise views and style login page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
+  authenticated :user do
+    root "pages#my_restaurants", as: :authenticated_root
+  end
   root 'pages#index'
 
   namespace :api do
